c21aa92d3ec6e66535bc192d5cbcfd8dddedb23c
Minoru-Abe <tomominoru.abe@gmail.com>
2024-11-14 20:21:51 +0900

    props

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
+import { ColorfulMessage } from "./components/ColorfulMessage";
+
 export const App = () => {
   const onClickButton = () => alert();
-  const contentStyle = {color: "red"};
   return (
     <div>
-      <h1 style={contentStyle}>こんにちは！</h1>
-      <p>お元気ですか</p>
+      <h1 style={{color:"red"}}>こんにちは！</h1>
+      <ColorfulMessage color="blue">お元気ですか</ColorfulMessage>
+      <ColorfulMessage color="green">元気です</ColorfulMessage>
       <button onClick={onClickButton}>ボタン</button>
     </div>
   );
